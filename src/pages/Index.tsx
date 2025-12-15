1b4b2ec2667fc4ebeccc12596b7e6ae0878e2a6e
Kyoz <banminkyoz@gmail.com>
2025-12-15 09:06:27 +0700

    init

---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { BirthdayWish } from '@/components/BirthdayWish'
+import { TabsSection } from '@/components/TabsSection'
+import { MessagesList } from '@/components/MessagesList'
+import { Gallery } from '@/components/Gallery'
+import { Footer } from '@/components/Footer'
+import { FloatingElements } from '@/components/FloatingElements'
+import { SparkleAnimation } from '@/components/SparkleAnimation'
+import { ScrollToTop } from '@/components/ScrollToTop'
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background relative">
+      <FloatingElements />
+      <SparkleAnimation />
+      <main>
+        <Hero />
+        <Gallery />
+      </main>
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default Index;
